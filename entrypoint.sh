c790011c4c67970c81596314795fc3c20558fd62
J C <39567962+jegalchoi@users.noreply.github.com>
2022-11-02 16:20:06 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -e
-
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pidbundle
-
 exec rake db:exists || bundle exec rake db:create db:migrate
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
